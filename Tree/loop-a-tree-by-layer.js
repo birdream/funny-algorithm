a3e799ceb8691bf6264f5e8794b9fec4008d9a29
nomran <nochen@xogrp.com>
2019-06-24 19:10:29 +0800

    stash

---
+++ Tree/loop-a-tree-by-layer.js
@@ -0,0 +1,27 @@
+// 广度优先算法 BFS
+const solution_BFS = (node) => {
+    if (!node) { return [] }
+
+    const result = [];
+    const queue = [];
+    queue.push(root);
+
+    // const visited = set(root)
+
+    while (queue.length) {
+        const level_size = queue.length;
+        const current_level = [];
+
+        for (let i = 0; i < level_size; i++) {
+            const element = array[i];
+
+        }
+    }
+}
+
+// 深度优先 DFS
+const solution_DFS = () => {}
+
+;(() => {
+
+})()
